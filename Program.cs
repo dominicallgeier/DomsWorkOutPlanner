e45c9f996b224ff04c3b0db82b7009172c561ae4
Dominic Allgeier <dfallg01@louisville.edu>
2022-03-07 19:58:08 -0500

    Added new path for txt files

--- Program.cs
+++ Program.cs
@@ -7,31 +7,41 @@ namespace DomsWorkOutPlanner
 {
     class Program
     {
-       static List<string> responses = new List<string>();
+        static List<string> responses = new List<string>();
         
-    static void Main(string[] args)
+        
+        
+
+
+        static void Main(string[] args)
         {
             bool showMenu = true;
             while (showMenu)
             {
                 showMenu = MainMenu();
             }
-           
+
+            
+
         }
         private static bool MainMenu()
         {
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("*************************************");
-            
+
             Console.WriteLine("Welcome to Dominic's Fitness Planner!");
-            
+
             Console.WriteLine("*************************************");
-            
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
             Console.WriteLine("*************************************");
-            
+
             Console.WriteLine("Create your own work out!");
-            
+
             Console.WriteLine("*************************************");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine();
             Console.WriteLine("Please select a category you want to work out:");
             Console.WriteLine("1) Legs");
@@ -39,7 +49,7 @@ namespace DomsWorkOutPlanner
             Console.WriteLine("3) Cardio");
             Console.WriteLine("4) Check your BMI");
             Console.WriteLine("5) EXIT");
-            
+
 
             switch (Console.ReadLine())
             {
@@ -65,8 +75,14 @@ namespace DomsWorkOutPlanner
 
         private static string CaptureInput()
         {
+            
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            
             Console.Write("Enter the work out you want: ");
+            Console.ResetColor();
             return Console.ReadLine();
+            
+            
         }
 
         public static void Legs()
@@ -99,7 +115,7 @@ namespace DomsWorkOutPlanner
             Console.WriteLine("Type which cardio exercise you would like to do:");
             Console.WriteLine("Run, Cycling or Swimming");
 
-            
+
 
             string yourWorkOut = CaptureInput();
             responses.Add(yourWorkOut);
@@ -108,51 +124,62 @@ namespace DomsWorkOutPlanner
 
         private static void DisplayResult(string message)
         {
+
             Console.WriteLine();
             Console.WriteLine($"You chose: {message}");
             if (message.ToLower() == "squat")
             {
-                string squatTxt = System.IO.File.ReadAllText(@"C:\WorkOutTxt\Legs\Squat.txt");
+                var squatTxtFileName = System.IO.Directory.GetCurrentDirectory() + @"\Txt\Legs\Squat.txt";
+                string squatTxt = System.IO.File.ReadAllText(squatTxtFileName);
+               
                 Console.WriteLine("\n" + squatTxt);
             }
             else if (message.ToLower() == "leg press")
             {
-                string legPressTxt = System.IO.File.ReadAllText(@"C:\WorkOutTxt\Legs\LegPress.txt");
+                var legPressTxtFileName = System.IO.Directory.GetCurrentDirectory() + @"\Txt\Legs\LegPress.txt";
+                string legPressTxt = System.IO.File.ReadAllText(legPressTxtFileName);
                 Console.WriteLine("\n" + legPressTxt);
             }
             else if (message.ToLower() == "lunges")
             {
-                string lungesTxt = System.IO.File.ReadAllText(@"C:\WorkOutTxt\Legs\Lunges.txt");
+                var lungesTxtFileName = System.IO.Directory.GetCurrentDirectory() + @"\Txt\Legs\Lunges.txt";
+                string lungesTxt = System.IO.File.ReadAllText(lungesTxtFileName);
                 Console.WriteLine("\n" + lungesTxt);
             }
             else if (message.ToLower() == "bench press")
             {
-                string benchTxt = System.IO.File.ReadAllText(@"C:\WorkOutTxt\Arms\BenchPress.txt");
+                var benchTxtFileName = System.IO.Directory.GetCurrentDirectory() + @"\Txt\Arms\BenchPress.txt";
+                string benchTxt = System.IO.File.ReadAllText(benchTxtFileName);
                 Console.WriteLine("\n" + benchTxt);
             }
             else if (message.ToLower() == "bicep curls")
             {
-                string bicepsTxt = System.IO.File.ReadAllText(@"C:\WorkOutTxt\Arms\BicepCurls.txt");
+                var bicepTxtFileName = System.IO.Directory.GetCurrentDirectory() + @"\Txt\Arms\BicepCurls.txt";
+                string bicepsTxt = System.IO.File.ReadAllText(bicepTxtFileName);
                 Console.WriteLine("\n" + bicepsTxt);
             }
             else if (message.ToLower() == "incline press")
             {
-                string inclineTxt = System.IO.File.ReadAllText(@"C:\WorkOutTxt\Arms\InclinePress.txt");
+                var inclineTxtFileName = System.IO.Directory.GetCurrentDirectory() + @"\Txt\Arms\InclinePress.txt";
+                string inclineTxt = System.IO.File.ReadAllText(inclineTxtFileName);
                 Console.WriteLine("\n" + inclineTxt);
             }
             else if (message.ToLower() == "run")
             {
-                string runTxt = System.IO.File.ReadAllText(@"C:\WorkOutTxt\Cardio\Run.txt");
+                var runTxtFileName = System.IO.Directory.GetCurrentDirectory() + @"\Txt\Cardio\Run.txt";
+                string runTxt = System.IO.File.ReadAllText(runTxtFileName);
                 Console.WriteLine("\n" + runTxt);
             }
             else if (message.ToLower() == "cycling")
             {
-                string cyclingTxt = System.IO.File.ReadAllText(@"C:\WorkOutTxt\Cardio\Cycling.txt");
+                var cyclingTxtFileName = System.IO.Directory.GetCurrentDirectory() + @"\Txt\Cardio\Cycling.txt";
+                string cyclingTxt = System.IO.File.ReadAllText(cyclingTxtFileName);
                 Console.WriteLine("\n" + cyclingTxt);
             }
             else if (message.ToLower() == "swimming")
             {
-                string swimmingTxt = System.IO.File.ReadAllText(@"C:\WorkOutTxt\Cardio\Swimming.txt");
+                var swimmingTxtFileName = System.IO.Directory.GetCurrentDirectory() + @"\Txt\Cardio\Swimming.txt";
+                string swimmingTxt = System.IO.File.ReadAllText(swimmingTxtFileName);
                 Console.WriteLine("\n" + swimmingTxt);
             }
             else
@@ -170,7 +197,7 @@ namespace DomsWorkOutPlanner
         public static void checkBMI()
         {
             Console.Clear();
-            
+
 
             double meters = .0254;
             double kG = 0.45;
@@ -186,24 +213,32 @@ namespace DomsWorkOutPlanner
             Console.WriteLine($"Your BMI: {Math.Round(BMI)}");
             if (BMI < 18.5)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("You are underweight.");
+                Console.ResetColor();
             }
             else if (BMI >= 18.5 && BMI <= 24.9)
             {
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("You are normal weight.");
+                Console.ResetColor();
             }
             else if (BMI >= 25 && BMI <= 29.9)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("You are overweight.");
+                Console.ResetColor();
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("You are obese.");
+                Console.ResetColor();
             }
             Console.WriteLine("\nPress ENTER for main menu.");
             Console.ReadKey();
 
-            
+
 
 
 
@@ -218,13 +253,42 @@ namespace DomsWorkOutPlanner
             {
                 Console.WriteLine();
                 Console.WriteLine("-" + x.ToUpper());
+                
+                
                 Console.WriteLine();
 
-                
+
             }
-                    
+
+            Console.WriteLine();
+            Quote();
+
+
+
+        }
+
+        public static void Quote()
+        {
+            var random = new Random();
+            List<string> quotes = new List<string>(5);
+            quotes.Add("“Energy & persistence conquer all things.” – Benjamin Franklin");
+            quotes.Add("“Make each day your masterpiece.” – John Wooden");
+            quotes.Add("“Life is like riding a bicycle. To keep your balance, you must keep moving.” – Albert Einstein");
+            quotes.Add("“Tough times never last, but tough people do.” – Dr. Robert Schuller");
+            quotes.Add("“Don’t wait, the time will never be just right.” – Napoleon Hill");
+
+            int i = random.Next(quotes.Count);
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine(quotes[i]);
+            Console.ResetColor();
 
 
         }
     }
+
+    
+    
+        
+    
 }
+
